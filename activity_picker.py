b2b793d981e017ee87aa8b39a00e1ad572695810
Bell Codo <bellcodo@gmail.com>
2016-01-29 17:20:46 -0800

    Update activity_picker.py

--- activity_picker.py
+++ activity_picker.py
@@ -1,9 +1,10 @@
 # import the magic random picker
-import random
+import random, urllib2
 
 
 # list of things to pick from
 activities_to_do = ['a','b','c']
+activities_to_do = urllib2.urlopen('https://raw.githubusercontent.com/bellcodo/potential-octo-chainsaw/master/code_activities_we_like.lst').read()
 
 
 # find a way to pick something
@@ -15,3 +16,5 @@ going_to_do = random.choice(activities_to_do)
 print "We can do the following " + str(activities_to_do)
 print "We are going to do " + going_to_do
 
+debug_example = "DEBUG: showing how it works typing.io\ncodeskulptor\ncodecombat\ncodeacademy\nscratch\nkaggle\ntynker?\n"
+print debug_example.split()
