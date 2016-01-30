15142655a8f4faa26d381ca00458fa4fcd8c2423
Bell Codo <bellcodo@gmail.com>
2016-01-29 16:44:32 -0800

    Create activity_picker.py

---
+++ activity_picker.py
@@ -0,0 +1,17 @@
+# import the magic random picker
+import random
+
+
+# list of things to pick from
+activities_to_do = ['a','b','c']
+
+
+# find a way to pick something
+going_to_do = activities_to_do[2]
+
+
+
+# a way to tell the user what to do
+print "We can do the following " + str(activities_to_do)
+print "We are going to do " + going_to_do
+
